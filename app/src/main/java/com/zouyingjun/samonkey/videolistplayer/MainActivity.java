d7b5563bbae07de8d5cc39d1ba60f0355bb42b68
zouyingjun <zouyingjun@samonkey.com>
2017-02-22 16:40:29 +0800

    Initial commit 导入ijk库

---
+++ app/src/main/java/com/zouyingjun/samonkey/videolistplayer/MainActivity.java
@@ -0,0 +1,13 @@
+package com.zouyingjun.samonkey.videolistplayer;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
